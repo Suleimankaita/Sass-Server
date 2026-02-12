a54b38525244912298bf49f868bd07f2635f8eff
suleiman yusuf kaita <suleiman20015kaita@gmail.com>
2026-02-12 11:15:11 +0100

    case

--- Routes/refresh.js
+++ Routes/refresh.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { refreshToken } = require("../controllers/refreshtoken");
+const { refreshToken } = require("../Controllers/refreshtoken");
 const  Verify = require("../Middleware/Verify");
 
 router.post("/refresh", refreshToken);
